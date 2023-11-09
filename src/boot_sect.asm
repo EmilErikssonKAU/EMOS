31ec5d18777cbb639deae352dfb67de6d65b65b4
EmilEriksson <emileriksson432@gmail.com>
2023-11-09 15:31:58 +0100

    Removed infinite loop

--- src/boot_sect.asm
+++ src/boot_sect.asm
@@ -4,12 +4,8 @@ bits 16                 ;
 main:
     hlt                 ;execution stops until interrupt
 
-.halt:                  ;local lable
-    jmp .halt            ;infinite loop, in case of interrupt
-
                         ;($-$$) <-> length of the program so far
 times 510 -($ -$$) db 0 ;fills the program with empty bits up until
                         ;position 510 with respect to start of sector
 
 dw 0xAA55               ;bootloader signature
-
