e271bebe976c514bbdcbf9cc13ea5156540dfe08
EmilEriksson <emileriksson432@gmail.com>
2023-11-10 01:49:56 +0100

    Greet restructuring

--- src/boot_sect.asm
+++ src/boot_sect.asm
@@ -2,10 +2,18 @@ org 0x7C00              ;according to BIOS standard
 bits 16                 ;
 
 main:
-    mov ah, 0x0e        ;BIOS interrupt code for typing to teletype
-                        ;int 0x10 -> interrupt for BIOS services
-                        ;the interrupt list is provided by BIOS
+    jmp greet
+    hlt                 ;execution stops until interrupt
+                        ;jmp $ <-> alternative for inf loop
 
+
+greet:
+    push bp		        ;prolog
+    mov sp, bp		    ;
+
+    mov ah, 0x0e        ;BIOS interrupt code for typing to teletype
+                        ;int 0x10 -> interrupt for BIOS video services
+                        ;the interrupt list is provided by BIOS
     mov al , 'G'
         int 0x10
     mov al , 'O'
@@ -26,9 +34,10 @@ main:
         int 0x10
     mov al , '!'
         int 0x10
-
-    hlt                 ;execution stops until interrupt
-                        ;jmp $ <-> alternative for inf loop
+    
+    mov bp, sp		    ;epilog
+    pop bp		        ;
+    ret			        ;
 
                         ;($-$$) <-> length of the program so far
 times 510 -($ -$$) db 0 ;fills the program with empty bits up until
